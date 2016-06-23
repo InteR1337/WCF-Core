2ccd1d692a6d6b53d356b5c1b546590b3ce59985
InteR1337 <interkst1337@gmail.com>
2016-06-23 23:35:47 +0300

    Adding WCF-service

---
+++ WCF-Core/GeoLib.Services/GeoManager.cs
@@ -0,0 +1,37 @@
+﻿using GeoLib.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoLib.Data;
+
+namespace GeoLib.Services
+{
+    public class GeoManager : IGeoService
+    {
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            ZipCodeData zipCodeData = null;
+
+            IZipCodeRepository zipCodeRepo = new ZipCodeRepository();
+
+            return zipCodeData;
+        }
+
+        public IEnumerable<string> GetStates(bool primaryOnly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
