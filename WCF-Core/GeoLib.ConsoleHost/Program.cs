c7445926fafaf64064ee9c48a41b2343268294fd
InteR1337 <interkst1337@gmail.com>
2016-06-25 13:20:46 +0300

    After Metadata Exchange module

--- WCF-Core/GeoLib.ConsoleHost/Program.cs
+++ WCF-Core/GeoLib.ConsoleHost/Program.cs
@@ -15,8 +15,29 @@ namespace GeoLib.ConsoleHost
     {
         static void Main(string[] args)
         {
-            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager), 
+                new Uri("http://localhost:8080"), 
+                new Uri("net.tcp://localhost:8009"));
+
+            ServiceMetadataBehavior metadataBehavior = hostGeoManager.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (metadataBehavior == null)
+            {
+                metadataBehavior = new ServiceMetadataBehavior();
+                metadataBehavior.HttpGetEnabled = true;
+                hostGeoManager.Description.Behaviors.Add(metadataBehavior);
+            }
+            else
+            {
+                metadataBehavior.HttpGetEnabled = true;
+            }
+
+            hostGeoManager.AddServiceEndpoint(typeof(IMetadataExchange), 
+                MetadataExchangeBindings.CreateMexHttpBinding(), "MEX");
+
             hostGeoManager.Open();
+
+
+
 
             Console.WriteLine("Services started. Press [Enter] to exit.");
             Console.ReadLine();
